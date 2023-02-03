c4a4d763eba7a27847c8090de9c3c5c368f80f6b
Vitor <direcaoass@gmail.com>
2023-02-03 12:21:03 -0300

    basic functions created

--- src/script.js
+++ src/script.js
@@ -1,2 +1,33 @@
 import './assets/css/style.css';
+import createContact from './modules/contact.js'
+import createHome from './modules/home.js';
+import createMenu from './modules/menu.js'
 
+
+
+(function createPage() {
+    const divContent = document.getElementById('content')
+    let mainContent
+    const header = `<div class="header"><h1>header</h1><div class='menu'><ul><li id='home'><a href="">Home</a></li> <li id='menu'><a href="">Menu</a></li><li id='contact'><a href="">Contact</a></li></ul></div></div>`;
+
+    setMainContent(createHome())
+    getNavOption();
+
+
+    function setMainContent(content) {
+        mainContent = `<div class="mainContent">${content}</div>`;
+        divContent.innerHTML = header + mainContent;
+    }
+
+    function getNavOption() {
+        const homeBtn = document.getElementById('home');
+        const menuBtn = document.getElementById('menu');
+        const contactBtn = document.getElementById('contact');
+
+        homeBtn.addEventListener('click', () => setMainContent(createHome()));
+        menuBtn.addEventListener('click', () => setMainContent(createMenu()));
+        contactBtn.addEventListener('click', () => setMainContent(createContact()));
+
+    }
+
+})()
