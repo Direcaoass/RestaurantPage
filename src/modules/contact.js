199657d658b5054c46ffa0aa93f2f87f230e025e
Vitor <direcaoass@gmail.com>
2023-02-05 14:50:30 -0300

    contact page added

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,5 +1,5 @@
 export default function createContact() {
-    const contact = `<h2>Contact Form</h2>`
+    const contact = `<h2><i class="fa-solid fa-phone"></i> +5547987456321<br> <br><i class="fa-solid fa-location-dot"></i> 45 Lexington St, Carnaby, London</h2> <br><img class='img' src="./assets/img/contact.jpg" alt="contactimg">`
 
     return contact;
 }
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,10 +1,10 @@
 export default function createMenu() {
 
-        let menuBlocks = ''
+        let menuItem = ''
     for (let i = 1; i < 9; i++) {
-        menuBlocks += `<div class='block'><img class='img' src="./assets/img/img${i}.jpg" alt="img${i}"><h4>SandwichS andwichSa ndwichS andwi chSan dw ich ${i}</h4></div>`;
+        menuItem += `<div class='menuItem'><img class='img' src="./assets/img/img${i}.jpg" alt="img${i}"><h4>SandwichS andwichSa ndwichS andwi chSan dw ich ${i}</h4></div>`;
     }
-    let menuContent = `<div class='menuContent'>${menuBlocks}</div>`;
+    let menuContent = `<div class='menuContent'>${menuItem}</div>`;
 
     return menuContent;
 
